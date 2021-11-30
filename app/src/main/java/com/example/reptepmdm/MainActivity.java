45fb25a4894293cb4822072f9bbe3d22b6e3472a
Francesc Bagué <fbaguem2021@cepnet.net>
2021-11-30 09:30:11 +0100

    He continuat amb la declaració dels objectes de la llista

--- app/src/main/java/com/example/reptepmdm/MainActivity.java
+++ app/src/main/java/com/example/reptepmdm/MainActivity.java
@@ -14,11 +14,29 @@ public class MainActivity extends AppCompatActivity {
     private Vivenda[] getivendes(){
 
         Vivenda[] vivendes = {
-                new Pis("Pis", 0,  "Barcelona - Sarrià - Sant Gervasi", "Avinguda de Sarrià, 246", "3200 €/mes", "5 hab, 3 banys, 154 m2", "No llogat",
+                new Pis("Pis", 0, "Barcelona - Sarrià - Sant Gervasi", "Avinguda de Sarrià, 246", "3200 €/mes", "5 hab, 3 banys, 154 m2", "No llogat",
                         "4rt", "2ª", "Amb ascensor"),
-                //new Atic()
+                new Atic("Àtic",0, "Argentona", "Francesc Macià, 25", "825 €/mes", "3 hab, 3 banys, 78 m2", "No llogat",
+                        "7(real)", "1", "si", "no"),
+                new Pis( "Pis", 0, "Barcelona: Eixample Dret", "Diputació, 736", "1050 €/mes", "4 hab, 2 banys, 104 m2", "Llogater: Friedrich W. Ölbert",
+                        "2", "2", "no"),
+                new Pis( "Pis", 0, "Barcelona - Les Tres Torres", "Via Augusta, 279", "1650 €/mes", "2 hab, 2 banys, 99 m2", "2 hab, 2 banys, 99 m2",
+                        "3", "3", "si"),
+                new Casa("Casa", 0, "Navata", "Carretera d’Olot, 23", "800 €/mes", "3 hab, 1 banys, 100 m2", "Llogater: Anna Pérez",
+                        "25m2", "13m2", " 5,30"),
+                new Atic("Pis", 0, "", "", "", "", "",
+                        "", "", "", "")
         };
 
         return vivendes;
     }
 }
+/*
+
+new Pis( "Pis", 0, "", "", "", "", "",
+        "", "", "")
+new Casa("Casa", 0, "", "", "", "", "",
+        "", "", "")
+new Atic("Pis", 0, "", "", "", "", "",
+        "", "", "", "")
+*/
