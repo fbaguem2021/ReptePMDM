bf16ddcce71d9873d6601d3af024566252ee34af
fbaguem2021 <fbaguem2021@cepnet.net>
2021-11-30 20:52:30 +0100

    Subida de imagenes a drawable

--- app/src/main/java/com/example/reptepmdm/MainActivity.java
+++ app/src/main/java/com/example/reptepmdm/MainActivity.java
@@ -15,25 +15,26 @@ public class MainActivity extends AppCompatActivity {
         final Vivenda[] vivendes = getivendes();
 
         ListView listVivendes = findViewById(R.id.listVivendes);
-        
+        VivendaAdapter adapter = new VivendaAdapter(this, vivendes);
+        listVivendes.setAdapter(adapter);
     }
     private Vivenda[] getivendes(){
         Vivenda[] vivendes = {
-                new Pis("Pis", 0, "Barcelona - Sarrià - Sant Gervasi", "Avinguda de Sarrià, 246", "3200 €/mes", "5 hab, 3 banys, 154 m2", "No llogat",
+                new Pis("Pis", R.drawable.vivenda_1, "Barcelona - Sarrià - Sant Gervasi", "Avinguda de Sarrià, 246", "3200 €/mes", "5 hab, 3 banys, 154 m2", "No llogat",
                          "4rt", "2ª", "Amb ascensor"),
-                new Atic("Àtic",0, "Argentona", "Francesc Macià, 25", "825 €/mes", "3 hab, 3 banys, 78 m2", "No llogat",
+                new Atic("Àtic", R.drawable.vivenda_2, "Argentona", "Francesc Macià, 25", "825 €/mes", "3 hab, 3 banys, 78 m2", "No llogat",
                          "7(real)", "1", "Si", "No"),
-                new Pis( "Pis", 0, "Barcelona: Eixample Dret", "Diputació, 736", "1050 €/mes", "4 hab, 2 banys, 104 m2", "Llogater: Friedrich W. Ölbert",
+                new Pis( "Pis", R.drawable.vivenda_3, "Barcelona: Eixample Dret", "Diputació, 736", "1050 €/mes", "4 hab, 2 banys, 104 m2", "Llogater: Friedrich W. Ölbert",
                          "2", "2", "No"),
-                new Pis( "Pis", 0, "Barcelona - Les Tres Torres", "Via Augusta, 279", "1650 €/mes", "2 hab, 2 banys, 99 m2", "2 hab, 2 banys, 99 m2",
+                new Pis( "Pis", R.drawable.vivenda_4, "Barcelona - Les Tres Torres", "Via Augusta, 279", "1650 €/mes", "2 hab, 2 banys, 99 m2", "2 hab, 2 banys, 99 m2",
                          "3", "3", "Si"),
-                new Casa("Casa", 0, "Navata", "Carretera d’Olot, 23", "800 €/mes", "3 hab, 1 banys, 100 m2", "Llogater: Anna Pérez",
+                new Casa("Casa", R.drawable.vivenda_5, "Navata", "Carretera d’Olot, 23", "800 €/mes", "3 hab, 1 banys, 100 m2", "Llogater: Anna Pérez",
                          "25m2", "13m2", " 5,30"),
-                new Atic("Atic", 0, "Santa Coloma de Farners", "Francesc Moragas, 14", "750 €/mes", "3 hab, 2 banys, 85 m2", "No llogat",
+                new Atic("Atic", R.drawable.vivenda_6, "Santa Coloma de Farners", "Francesc Moragas, 14", "750 €/mes", "3 hab, 2 banys, 85 m2", "No llogat",
                          "6", "2", "No", "No"),
-                new Pis( "Pis", 0, "Barcelona - Eixample Esquerre", "Aragó, 235", "1100 €/mes", "3 hab, 2 banys, 100 m2", "No llogat",
+                new Pis( "Pis", R.drawable.vivenda_7, "Barcelona - Eixample Esquerre", "Aragó, 235", "1100 €/mes", "3 hab, 2 banys, 100 m2", "No llogat",
                          "5", "2", "Si"),
-                new Pis( "Pis", 0, "Barberà del Vallès", "Avinguda de la Generalitat, 83", "985 €/mes", "3 hab, 2 banys, 92 m2", "Llogater: Joan Calvet",
+                new Pis( "Pis", R.drawable.vivenda_8, "Barberà del Vallès", "Avinguda de la Generalitat, 83", "985 €/mes", "3 hab, 2 banys, 92 m2", "Llogater: Joan Calvet",
                          "5", "2", "No")
         };
         return vivendes;
